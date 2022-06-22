a0194807ba4e3ea60eadc14454f6e6fab5508dd9
mgrmars <88881293+mgrmars@users.noreply.github.com>
2022-06-21 23:57:54 -0500

    Update FileReadWrite.java

--- File/FileReadWrite.java
+++ File/FileReadWrite.java
@@ -1,114 +1,109 @@
 package File;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
+/**
+ * @author Miguel A. Gonzales R.
+ */
 public class FileReadWrite {
 
     public static void main(String[] args) {
-        
-        boolean life = true;
-        int option;
-	    Scanner in = new Scanner(System.in);
-        
-	    System.out.println("1. Crear y (sobre)escribe\n2. Agregar texto\n3. Leer\n0. Salir");
-        option = in.nextInt();
-        in.nextLine(); // Previene un errorcito del Scanner.
-        
-	    while (life) {
-            
-	        switch (option) {       
-		        case 0:
-                    life = false;
-                    break;
-
-                case 1:
-                    System.out.print("Ingresa texto: ");
-                    String wrt = in.nextLine();
-                    bWriter(wrt);
-                    life = false;
-                    break;
-
-                case 2:
-                    System.out.print("Ingresa texto: ");
-                    String apnd = in.nextLine();
-                    life = false;
-                    break;
-
-                case 3:
-                    bReader();
-                    System.out.println("Ingresa cualquier tecla para salir");
-                    in.nextLine();
-                    life = false;
-                    break;
-
-                default:
-                    System.out.println("Opcion incorrecta");
-            }
-        }
+        bAppend("texto");
+        bReader();
+        System.out.println(countLines());
     }
 
-    // Lee el contenido del archivo.
+   /**
+     * Lee y Muestra Lineas.
+     * Lee y muestra las lineas de un archivo.
+     * @param arg Texto para escribir
+     */
     public static void bReader() {
-
-	    try {
-            File file = new File("file.txt");
-            FileReader fileReader = new FileReader(file); // A stream that connects to the text file
+        try {
             String line;
+            // Lector
+            File file = new File("file.txt");
+            FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader); // Connect the FileReader to the BufferedReader
-            	
-		    while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line); // Display the file's contents on the screen, one line at a time
+            // Recorre cada linea del archivo mientras su contenido no sea nulo
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
             }
-
-            bufferedReader.close(); // Close the stream
-        
+            bufferedReader.close();
         } catch (IOException e) {
             System.out.println(e);
         }
     }
 
-    // Crea y/o sobreescribe el archivo.
+    /**
+     * Escribe Lineas.
+     * Escribe texto en un archivo, no necesariamente existente.
+     * @param arg Texto para escribir
+     */
     public static void bWriter(String arg) {
-        
         try {
-        
+            String line;
             File file = new File("file.txt");
-            FileWriter fileWriter = new FileWriter(file); // A stream that connects to the text file
-            
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter); // Connect the FileWriter to the BufferedWriter
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             bufferedWriter.write(arg + "\n");
-            
-            bufferedWriter.close(); // Close the stream
-        
+            bufferedWriter.close();
         } catch (IOException e) {
             System.out.println(e);
         }
     }
 
-    
-    
-    // Se supone que esta función deberia agregar texto 
-    // sin reemplazar lo que ya contiene el archivo.
-    // No lo hace por si acaso.
-   
-    /*
-    public static void bAppender(String arg) {
+    /**
+     * Leer Lineas.
+     * Lee cuantas lineas tiene un archivo existente.
+     * @return Devuelve la cantidad de lineas del archivo.
+     */
+    public static int countLines() {
+        int n = 0;
         try {
+            String line;
+            // Inicia el archivo
             File file = new File("file.txt");
-            FileWriter fileWriter = new FileWriter(file); // A stream that connects to the text file
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter); // Connect the FileWriter to the BufferedWriter
-            bufferedWriter.write(arg + "\n");            
-            bufferedWriter.close(); // Close the stream
+            // Inicia el escritor
+            FileReader fileReader = new FileReader(file);
+            // Lee el archivo
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            try {
+                while ((line = bufferedReader.readLine()) != null) {
+                    n++;
+                }
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
+            return n;
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } 
+        return n;
+    }
+
+    /**
+     * Agrega lineas.
+     * Agrega lineas a un archivo existente.
+     * @param arg Texto para agregar.
+     */
+    public static void bAppend(String arg) {
+        try {
+            // Inicializa y define el archivo.
+            File file = new File("file.txt");
+            // Iniciar el escritor en modo agregar (append)
+            // FileWriter(file, TRUE <-----);
+            // Flujo que conecta FileWriter al archivo
+            FileWriter fileWriter = new FileWriter(file, true);
+            // Conecta FileWriter a BufferedWriter
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter); 
+            // Escribe, inicia el flujo.
+            bufferedWriter.write(arg + "\n");
+            // Cierra el flujo, hace un .flush() de forma automática.
+            bufferedWriter.close();
         } catch (IOException e) {
             System.out.println(e);
         }
     }
-    */
 }
-
