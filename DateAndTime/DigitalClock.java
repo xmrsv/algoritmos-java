d75ab120fbe9904bbb17e674ea39792d06d31e79
mgrmars <88881293+mgrmars@users.noreply.github.com>
2022-07-06 18:05:07 -0500

    Update DigitalClock.java

--- DateAndTime/DigitalClock.java
+++ DateAndTime/DigitalClock.java
@@ -6,32 +6,10 @@ import java.util.Calendar;
 public class DigitalClock {
 
     public static void main(String[] args) {
-        setTime('s');
+        setTime();
     }
 
-    public static void setTime(char type) {
-
-        int second = 1000;
-        int minute = 60000;
-        int hour = 3600000;
-        
-        switch (type) {
-            case 's': // 1 second
-                timeCalc(second);
-                break;
-            case 'm': // 1 minute
-                timeCalc(minute);
-                break;
-            case 'h': // 1 hour
-                timeCalc(hour);
-                break;
-            default:
-                System.out.println("Invalid type");
-        }
-        
-    }
-
-    public static void timeCalc(int type) {
+    public static void setTime() {
         Calendar calendar;
         SimpleDateFormat timeFormat;
         String time;
@@ -39,7 +17,7 @@ public class DigitalClock {
 
         while (true) {
             time = timeFormat.format(Calendar.getInstance().getTime());
-            System.out.println(time);
+            System.out.println(60000);
             try {
                 Thread.sleep(type);
             } catch (InterruptedException e) {
