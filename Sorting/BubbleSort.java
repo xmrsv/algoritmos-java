9e82cc15fa45d47df67c181fdef13c7d9b9f66cd
mgrmars <88881293+mgrmars@users.noreply.github.com>
2022-07-07 09:22:58 -0500

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -4,69 +4,18 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         
-        int[] numeritos = {5, 1, 3, 7, 9, 0, 8, 4, 2, 6};
-        int[] numeritosOrdenados;
-        
-        /**
-         * Ordenamos el array y lo almacenamos en otra variable
-         * Podemos reemplazar el mismo array pero lo haremos de forma ordenada
-         */
-        numeritosOrdenados = algoritmoBurbuja(numeritos);
-        
-        // Mostrar elementos del array
-        for(int numerito: numeritosOrdenados) {
-            System.out.print(numerito);
-        }
-        
-        // Salto de linea, no es necesario
-        System.out.println(""); 
     }
     
-    
-    /**
-     * 
-     * Esta funcion recibe un array de argumento
-     * y retorna un array ordenado con el algoritmo de ordenamiento Burbuja
-     * 
-     * @param array a ordenar
-     * @return array ordenado
-     * 
-     */
-    public static int[] algoritmoBurbuja(int[] array) {
-        
-        int temporal;
-        int arrayTamano;
-        
-        arrayTamano = array.length;
-
-        while (true) {
-            
-            boolean cambio = false;
-
-            for (int i = 1; i < arrayTamano; i++) {
-                /**
-                 * Comparamos el numero de la izquierda con el de la derecha
-                 * si el numero de la izquierda es mayor al de la derecha.
-                 *
-                 * 1. Almacenamos el valor de x1 (numero de la izquierda) en una variable t (temporal).
-                 * 2. Reemplazamos el valor de x2 (el numero de la derecha) por x1 (el numero de la izquierda).
-                 * 3. Reemplazamos el valor de x2 por el valor de t.
-                 * 4. Repetimos hasta que la condicion no se cumpla y llegemos al final del array.
-                 */ 
-                if (array[i - 1] > array[i]) {
-                    temporal = array[i];      
-                    array[i] = array[i - 1];
-                    array[i - 1] = temporal;
-                    cambio = true;
+    static void bubbleSort(int[] a, int n) {
+        int i, j, aux;
+        for (i = 0; i < n - 1; i++) {
+            for (j = 0; j < n - i - 1; j++) {
+                if (a[j + 1] < a[j]) {
+                    aux = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = aux;
                 }
             }
-            
-            // Rompemos el bucle while cuando lleguemos al final del array
-            if (cambio == false) {
-                break;
-            }
         }
-        // Es obvio pero retornamos el arreglo ordenado
-        return array; 
     }
 }
