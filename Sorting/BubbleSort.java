30b93c4406ab9c1f88fbfb17a81877a5aa239f48
mgrmars <88881293+mgrmars@users.noreply.github.com>
2022-07-07 09:34:53 -0500

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -3,10 +3,28 @@ package Sorting;
 public class BubbleSort {
 
     public static void main(String[] args) {
+
+        int[] numbers = {9, 0, 1, 8, 2, 7, 3, 6, 4, 5};
+        String[] names = {"Anais", "Gumball", "Darwin", "Nicolle", "Richard"};
         
+        bubbleInt(numbers, numbers.length);
+        bubbleString(names, names.length);
+        
+        for(int number: numbers) {
+            System.out.print(number + " ");
+        }
+        
+        System.out.println();
+        
+        for(String name: names) {
+            System.out.print(name + " ");
+        }
+        
+        System.out.println();
+
     }
-    
-    static void bubbleSort(int[] a, int n) {
+
+    static void bubbleInt(int[] a, int n) {
         int i, j, aux;
         for (i = 0; i < n - 1; i++) {
             for (j = 0; j < n - i - 1; j++) {
@@ -14,6 +32,21 @@ public class BubbleSort {
                     aux = a[j + 1];
                     a[j + 1] = a[j];
                     a[j] = aux;
+                }
+            }
+        }
+    }
+
+    static void bubbleString(String[] a, int n) {
+        String aux;
+        int i, j;
+
+        for (i = 1; i < n; i++) {
+            for (j = 0; j < n - 1; j++) {
+                if (a[j].compareTo(a[j + 1]) > 0) {
+                    aux = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = aux;
                 }
             }
         }
