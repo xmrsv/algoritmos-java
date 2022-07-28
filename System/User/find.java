be4c11905c2523589176f894b5044ac37e4f49af
mgrmars <88881293+mgrmars@users.noreply.github.com>
2022-07-27 22:22:03 -0500

    Update and rename findCredentials.java to find.java

---
+++ System/User/find.java
@@ -0,0 +1,21 @@
+public boolean find(int elementsAmount, int elementIndex, String filename, String element, String separator) {
+        int found = 0;
+        String line;
+        String[] elements = new String[elementsAmount];
+        try {
+            File file = new File(filename);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            while ((line = bufferedReader.readLine()) != null) {
+                StringTokenizer stringTokenizer = new StringTokenizer(line, separator);
+                for (int i = 0; (stringTokenizer.hasMoreTokens()); i++) {
+                    elements[i] = stringTokenizer.nextToken();
+                }
+                if (elements[elementIndex].equals(element)) {
+                    found++;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return found > 0;
+    }
