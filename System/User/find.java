eab9708e2b12673062571973fa0215a5d6c1e47b
mgrmars <88881293+mgrmars@users.noreply.github.com>
2022-07-27 22:25:00 -0500

    Update find.java

--- System/User/find.java
+++ System/User/find.java
@@ -8,8 +8,7 @@ public boolean find(int elementsAmount, int elementIndex, String filename, Strin
         while ((line = bufferedReader.readLine()) != null) {
             StringTokenizer stringTokenizer = new StringTokenizer(line, separator);
             for (int i = 0; (stringTokenizer.hasMoreTokens()); i++)  elements[i] = stringTokenizer.nextToken()
-            if (elements[elementIndex].equals(element)) found++;
-                
+            if (elements[elementIndex].equals(element)) found++
         }
     } catch (IOException e) {
         System.out.println(e);
