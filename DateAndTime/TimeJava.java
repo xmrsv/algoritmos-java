e209bbd8654bf45071c6c01408fd6532317ea801
xmrsv <88881293+xmrsv@users.noreply.github.com>
2023-05-25 16:13:29 -0500

    Update TimeJava.java
    Code reorganized

--- DateAndTime/TimeJava.java
+++ DateAndTime/TimeJava.java
@@ -6,28 +6,30 @@ import java.util.StringTokenizer;
 public class TimeJava {
 
     public static void main(String[] args) {
-        
+        String[] datas = new String[3];
         LocalTime time = LocalTime.now();
-        System.out.println("Hora sin formatear " + time);
+        
+        System.out.println("Formato (hh mm ss.ssssss)");
+        System.out.println("Hora sin formatear: \n" + time);
         
         StringTokenizer st = new StringTokenizer(String.valueOf(time), ":");
         
-        String[] datas = new String[3];
-        
-        System.out.print("Hora formateada: ");
+        System.out.println("Hora formateada (separadas en un array de strings, sin dos puntos [:]):");
         
         for (int i = 0; (st.hasMoreTokens()); i++) {
             datas[i] = st.nextToken();
             System.out.print(datas[i] + " ");
         }
-        System.out.print("\nHora formateada y limpiada: ");
+        
+        System.out.println();
+        
+        System.out.println("Hora formateada (segundos sin decimales): ");
         
         st = new StringTokenizer(datas[2], ".");
         datas[2] = st.nextToken();
     
-        for(String data: datas) {
-            System.out.print(data + " ");
-        }
+        for(String data: datas) System.out.print(data + " ");
+        
         System.out.println();
     }
 }  
